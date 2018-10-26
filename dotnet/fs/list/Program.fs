8d1eb47cabd3a05bc4b11ab39c7a39893f45014a
Ilya Druzhinin <ilya.druzh@gmail.com>
2018-10-26 20:25:22 +0300

    fs

---
+++ dotnet/fs/list/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
+
+let rec insertions x = function
+    [] -> [[x]]
+  | h::t -> (x::h::t)::(map (fun z -> h::z)(insertions x t));;  
