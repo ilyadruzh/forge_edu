2543d73d9fb28de49958943019160339fee853c1
Eliot Krone <krondev@pm.me>
2019-09-25 22:54:11 +0300

    sicp

---
+++ sicp/racket/1.3.rkt
@@ -0,0 +1,88 @@
+#lang racket
+
+(define (identity x) x)
+(define (inc n) (+ n 1))
+(define (cube x) (* x x x))
+(define (pi-term x) (/ 1.0 (* x (+ x 2))))
+(define (pi-next x) (+ x 4))
+(define (square x) (* x x))
+(define (average x y) (/ (+ x y) 2))
+(define (close-enough? x y) (< (abs (- x y)) 0.001))
+
+(define (sum-integers a b)
+  (if (> a b)
+      0
+      (+ a (sum-integers (+ a 1) b))))
+
+(define (sum-cubes1 a b)
+  (if (> a b)
+      0
+      (+ (cube a) (sum-cubes1 (+ a 1) b))))
+
+(define (pi-sum1 a b)
+  (if (> a b)
+      0
+      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (sum-cubes2 a b)
+  (sum cube a inc b))
+
+(define (pi-sum a b) (sum pi-term a pi-next b))
+
+(lambda (x) (/ 1.0 (* x (+ x 2))))
+
+(define (pi-sum2 a b)
+  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+       a
+       (lambda (x) (+ x 4))
+       b))
+
+(define (f x y)
+  (define (f-helper a b)
+    (+ (* x (square a))
+       (* y b)
+       (* a b)))
+  (f-helper (+ 1 (* x y))
+            (- 1 y)))
+
+(define (f1 x y)
+  ((lambda (a b)
+     (+ (* x (square a))
+        (* y b)
+        (* a b)))
+   (+ 1 (* x y))
+   (- 1 y)))
+
+(define (f2 x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else (error "У аргументов не разные знаки " a b)))))
+
