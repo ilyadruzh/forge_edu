141c66cd963ae01c9ae0d1422d2da8d9b729d147
Eliot Krone <krondev@pm.me>
2019-10-06 00:40:48 +0300

    otp init

---
+++ otp/queens.erl
@@ -0,0 +1,13 @@
+-module(queens).
+-export([queens/1]).
+
+queens(0) -> [[]];
+queens(N) ->
+    [[Row | Columns] || Columns <- queens(N-1), % -- возвратит разность списков
+        Row <- [1,2,3,4,5,6,7,8,9] -- Columns,
+        safe(Row, Columns, 1)].
+
+safe(_Row, [], _N) -> true;
+safe(Row, [Column|Columns],N) ->
+    (Row /= Column + N) andalso (Row /= Column - N) andalso 
+        safe(Row, Columns, (N+1)).
