18848a304cb63121ec08f846b0c8c59f16996c58
Eliot Krone <krondev@pm.me>
2020-02-18 20:00:25 +0300

    day12

--- hackerrank/30days/day11/main.exs
+++ hackerrank/30days/day11/main.exs
@@ -1,26 +1,52 @@
 defmodule Solution do
   # Enter your code here. Read input from STDIN. Print output to STDOUT
-
-  # берем общее количество эллемнтов, делим на 3 - получаем номер начаола нового блока и его инкремент
   def start() do
-    n =
-      IO.read(:stdio, :all)
-      |> String.trim()
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
-      |> (&Enum.chunk_every(&1, round(:math.sqrt(length(&1))))).()
-      |> Enum.map(fn x -> Enum.chunk_every(x, 3) end)
-      |> IO.inspect()
-      |> Enum.map(fn x -> Enum.sum(x) end)
-      |> Enum.max()
-      |> engine
+    IO.read(:stdio, :all)
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> engine
   end
 
   def engine(n) do
-    IO.inspect(n)
+    max =
+      Enum.at(n, 0) + Enum.at(n, 1) + Enum.at(n, 2) + Enum.at(n, 7) +
+        Enum.at(n, 12) + Enum.at(n, 13) + Enum.at(n, 14)
+
+    sum(n, max, 0)
   end
 
-  def line() do
+  def sum(nil, max, line) do
+    IO.puts(max)
+  end
+
+  def sum(n, max, line) do
+    [_ | tail] = n
+
+    cond do
+      Enum.at(n, 14) == nil ->
+        sum(nil, max, line + 1)
+
+      line == 4 || line == 5 || line == 10 || line == 11 || line == 16 || line == 17 || line == 22 ||
+          line == 23 ->
+        sum(tail, max, line + 1)
+
+      Enum.at(n, 14) != nil ->
+        result =
+          Enum.at(n, 0) + Enum.at(n, 1) + Enum.at(n, 2) + Enum.at(n, 7) +
+            Enum.at(n, 12) + Enum.at(n, 13) + Enum.at(n, 14)
+
+        cond do
+          result > max ->
+            sum(tail, result, line + 1)
+
+          result < max ->
+            sum(tail, max, line + 1)
+
+          result == max ->
+            sum(tail, max, line + 1)
+        end
+    end
   end
 end
 
