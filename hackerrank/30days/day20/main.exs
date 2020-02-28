679ffb1cd2d27316420bf9caec4e3419d375bfe0
Eliot Krone <krondev@pm.me>
2020-02-28 17:52:28 +0300

    update

--- hackerrank/30days/day20/main.exs
+++ hackerrank/30days/day20/main.exs
@@ -7,24 +7,50 @@ defmodule Solution do
       |> Enum.map(&String.to_integer/1)
 
     tail
-    |> sort([], 0, head)
+    |> sort()
   end
 
-  def sort([], result, count, step) when step == 0  do
-    IO.puts("Array is sorted in #{count} swaps.")
-    first_elem(result)
-    last_elem(result)
+  def sort(list) when is_list(list) do
+    make_pass(do_sort(list, [], 0), list)
   end
 
-  def sort(input, result, count, step) do
-    case hd(input) > hd(result) do
-      true -> sort(tl(input), result ++ [hd(input)], count = count + 1, step)
-      false -> sort(tl(input), [hd(input)] ++ result, count, step = step - 1)
+  def make_pass(bubbled_list, old_list) when bubbled_list != old_list do
+    do_sort(bubbled_list, [], 0) |> make_pass(bubbled_list)
+  end
+
+  def make_pass(bubbled_list, old_list) when tl(bubbled_list) == old_list do
+    IO.inspect(bubbled_list)
+    IO.inspect(old_list)
+
+    IO.puts("Array is sorted in #{hd(bubbled_list)} swaps.")
+    first_elem(tl(bubbled_list))
+    last_elem(bubbled_list)
+    # bubbled_list
+  end
+
+  def do_sort(_list = [], _acc, _count) do
+    []
+  end
+
+  def do_sort([first | []], acc, count) do
+    [count] ++ acc ++ [first]
+  end
+
+  def do_sort([first | [second | tail]], acc, count) do
+    [new_first, new_second, new_count] = swap(first, second, count)
+    do_sort([new_second | tail], acc ++ [new_first], new_count)
+  end
+
+  defp swap(e1, e2, count) do
+    if e1 <= e2 do
+      [e1, e2, count]
+    else
+      [e2, e1, count = count + 1]
     end
   end
 
   defp first_elem(input) do
-    n = hd(input)
+    n = hd(tl(input))
     IO.puts("First Element: #{n}")
   end
 
