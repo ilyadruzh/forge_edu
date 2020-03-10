afea1ba45c2ec0406e7871bbe542d2e3bad21f10
Eliot Krone <krondev@pm.me>
2020-03-11 00:32:05 +0300

    update

---
+++ kernighan-ritchie/ch-1.10.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+char result[MAXLINE];
+int tabwidth;
+
+int getmyline(void);
+void copy(void);
+
+main()
+{
+    int len;
+    extern int max;
+    extern char longest[];
+
+    max = 0;
+    while ((len = getmyline()) > 0)
+    {
+        if (len > max)
+        {
+            max = len;
+            copy();
+        }
+
+        if (max > 0)
+        {
+            detab(longest);
+            // printf("%s", longest);
+        }
+    }
+
+    // if (max > 0)
+    // {
+    //     printf("%s", longest);
+    // }
+    return 0;
+}
+
+int getmyline(void)
+{
+    int c, i;
+
+    extern char line[];
+
+    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    {
+        line[i] = c;
+    }
+    if (c == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+    return i;
+}
+
+void copy(void)
+{
+    int i;
+    extern char line[], longest[];
+
+    i = 0;
+
+    while ((longest[i] = line[i]) != '\0')
+    {
+        ++i;
+    }
+}
+
+void detab(char s[])
+{
+    extern char result[MAXLINE];
+    extern int tabwidth;
+
+    tabwidth = 4;
+
+    int result_len = 0;
+
+    for (int i = 0; i < MAXLINE - 1; ++i)
+    {
+        if (s[i] == '\t')
+        {
+            for (int x = 0; x <= tabwidth; ++x)
+            {
+                result[result_len + x] = '.';
+                printf("result_len: %d\n", result_len);
+            }
+            result_len = result_len + 4;
+        }
+        else
+        {
+            result[result_len] = s[i];
+            ++result_len;
+        }
+    }
+
+    printf("s: %s\n", s);
+    printf("result: %s\n", result);
+    // stack smashing detected ***: <unknown> terminated
+}
+
+void entab(char s[])
+{
+    // TODO: 1.21
+}
+
+void fold(char s[])
+{
+    // TODO: 1.22
+}
+
+void delete_comments(char s[])
+{
+    // TODO: 1.23
+}
+
+void syntax_check(char s[])
+{
+    // TODO:
+}
--- kernighan-ritchie/ch-1.9.c
+++ kernighan-ritchie/ch-1.9.c
@@ -4,15 +4,24 @@
 
 int getMyline(char line[], int maxline);
 void copy(char to[], char from[]);
+void reverse(char from[], char to[]);
 
 int main()
 {
     int len;
     int max;
+    int count;
     char line[MAXLINE];
     char longest[MAXLINE];
+    char goodlines[MAXLINE][MAXLINE];
+    char reverline[MAXLINE];
 
     max = 0;
+    count = 0;
+
+    int revert = 0;
+    int morethaneight = 0;
+
     while ((len = getMyline(line, MAXLINE)) > 0)
     {
         if (len > max)
@@ -20,9 +29,29 @@ int main()
             max = len;
             copy(longest, line);
         }
+
+        if (max > 0)
+        {
+            printf("longest: %s\n", longest);
+        }
+
+        if (morethaneight == 1 && len > 80)
+        {
+            copy(goodlines[count], line);
+            ++count;
+            for (int x = 0; x < count; ++x)
+            {
+                printf("more than 80 symbols[%d]: %s\n", x, goodlines[x]);
+            }
+        }
+        
+        if (revert == 1)
+        {
+            reverse(line, reverline);
+            printf("%s\n", reverline);
+        }
     }
-    if (max > 0)
-        printf("%s", longest);
+
     return 0;
 }
 
@@ -31,7 +60,10 @@ int getMyline(char s[], int lim)
     int c, i;
 
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    {
         s[i] = c;
+    }
+
     if (c == '\n')
     {
         s[i] = c;
@@ -49,6 +81,40 @@ void copy(char to[], char from[])
 
     while ((to[i] = from[i]) != '\0')
     {
+        if (from[i] == '\n')
+        {
+        }
         ++i;
     }
+}
+
+void reverse(char from[], char to[])
+{
+    int start = 0;
+    int x;
+
+    int endofline = 0;
+
+    for (int i = 0; i <= MAXLINE; ++i)
+    {
+        if (from[i] == '\n' || endofline == 1)
+        {
+            endofline = 1;
+            from[i] = 0;
+        }
+    }
+
+    for (x = MAXLINE; x >= 0; --x)
+    {
+        if (from[x] == '\n' || from[x] == '\0' || from[x] == 0)
+        {
+        }
+        else
+        {
+            to[start] = from[x];
+            ++start;
+        }
+    }
+
+    // to[start + 1] = '\0';
 }
